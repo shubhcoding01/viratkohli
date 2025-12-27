e90b149a4f8b789d17c0b684bebfaf527516ea13
shubhcoding01 <168171242+shubhcoding01@users.noreply.github.com>
2025-12-27 17:49:05 +0530

    Virat

--- src/components/canvas/TrophyModel.tsx
+++ src/components/canvas/TrophyModel.tsx
@@ -1,39 +1,3 @@
-// 'use client';
-
-// import { useRef } from 'react';
-// import { useFrame } from '@react-three/fiber';
-// import { useGLTF, Float, Sparkles } from '@react-three/drei';
-// import * as THREE from 'three';
-
-// export default function TrophyModel() {
-//   const { scene } = useGLTF('/models/trophy.glb');
-//   const ref = useRef<THREE.Group>(null);
-
-//   // Rotate the trophy slowly like a showcase
-//   useFrame((state, delta) => {
-//     if(ref.current) ref.current.rotation.y += delta * 0.5;
-//   });
-
-//   return (
-//     <group>
-//       <Float speed={2} rotationIntensity={0.5} floatIntensity={1}>
-//         <primitive 
-//           ref={ref} 
-//           object={scene} 
-//           scale={2} 
-//           position={[0, 0.5, 0]} 
-//         />
-        
-//         {/* Add Gold Sparkles around the Trophy */}
-//         <Sparkles count={50} scale={3} size={4} speed={0.4} opacity={1} color="#FFD700" />
-//       </Float>
-//     </group>
-//   );
-// }
-
-// useGLTF.preload('/models/trophy.glb');
-
-
 'use client';
 
 import { useRef, useState, useMemo } from 'react';
